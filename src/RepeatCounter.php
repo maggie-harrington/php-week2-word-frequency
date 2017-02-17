1e3b1f1f7b909783fc85280f3247b3430132475e
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-17 13:24:50 -0800

    pass test 9

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -25,14 +25,15 @@
             $inputWord = strtolower($inputWord);
             $inputString = strtolower($inputString);
 
-            $inputStringArray = explode(" ", $inputString);
+            $inputStringArray = str_word_count($inputString, 1);
+            // $inputStringArray = explode(" ", $inputString);
             $matchingArray = array();
             for ($i = 0; $i < sizeof($inputStringArray); $i++) {
                 if ($inputStringArray[$i] == $inputWord) {
                     array_push($matchingArray, $inputStringArray[$i]);
                 }
             }
-            var_dump($matchingArray);
+            // var_dump($matchingArray);
             $matchingString = implode(" ", $matchingArray);
             $repeatNumber = substr_count($matchingString, $inputWord);
 

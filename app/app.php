91e6c5f23a62dd4401a7986331c301dabd7e3e0a
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-17 14:05:06 -0800

    create route and initial html for home page

---
+++ app/app.php
@@ -0,0 +1,23 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app ['debug'] = true;
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->post("/results", function() use ($app) {
+        return $app['twig']->render('results.html.twig');
+    });
+
+    return $app;
+?>

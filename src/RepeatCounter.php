f3168db6683188659781fdfcec03c83e5fbeb6d0
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-17 13:10:35 -0800

    pass test 8

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -25,8 +25,19 @@
             $inputWord = strtolower($inputWord);
             $inputString = strtolower($inputString);
 
-            $repeatNumber = substr_count($inputString, $inputWord);
-            var_dump($repeatNumber);
+            $inputStringArray = explode(" ", $inputString);
+            $matchingArray = array();
+            for ($i = 0; $i < sizeof($inputStringArray); $i++) {
+                if ($inputStringArray[$i] == $inputWord) {
+                    array_push($matchingArray, $inputStringArray[$i]);
+                }
+            }
+            var_dump($matchingArray);
+            $matchingString = implode(" ", $matchingArray);
+            $repeatNumber = substr_count($matchingString, $inputWord);
+
+            // $repeatNumber = substr_count($inputString, $inputWord);
+            // var_dump($repeatNumber);
             return $repeatNumber;
         }
 
